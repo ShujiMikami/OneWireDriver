c1a74646761aed21f508f06799837e718a203457
ShujiMikami <carbonized.pan@gmail.com>
2018-07-13 03:19:55 +0900

    Waitのラッパーを作成

---
+++ OneWireTest/UserModules/OneWireDriver/Inc/Wait_Wrapper.h
@@ -0,0 +1,13 @@
+/*
+ * Wait_Wrapper.h
+ *
+ *  Created on: 2018/07/13
+ *      Author: Shuji
+ */
+
+#ifndef ONEWIREDRIVER_INC_WAIT_WRAPPER_H_
+#define ONEWIREDRIVER_INC_WAIT_WRAPPER_H_
+
+void Wait_us(unsigned short microSecond);
+
+#endif /* ONEWIREDRIVER_INC_WAIT_WRAPPER_H_ */
---
+++ OneWireTest/UserModules/OneWireDriver/Src/Wait_Wrapper.c
@@ -0,0 +1,12 @@
+/*
+ * Wait_Wrapper.c
+ *
+ *  Created on: 2018/07/13
+ *      Author: Shuji
+ */
+#include "Wait_Wrapper.h"
+
+__attribute__((weak)) void Wait_us(unsigned short microSecond)
+{
+
+}
