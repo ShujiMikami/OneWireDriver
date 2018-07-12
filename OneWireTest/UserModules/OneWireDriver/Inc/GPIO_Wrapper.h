2cb462159f225e16633540761f744449786a5da4
ShujiMikami <carbonized.pan@gmail.com>
2018-07-13 03:07:30 +0900

    GPIOの制御関数をweak属性にして汎用化

---
+++ OneWireTest/UserModules/OneWireDriver/Inc/GPIO_Wrapper.h
@@ -0,0 +1,16 @@
+/*
+ * GPIO_Wrapper.h
+ *
+ *  Created on: 2018/07/13
+ *      Author: Shuji
+ */
+
+#ifndef ONEWIREDRIVER_INC_GPIO_WRAPPER_H_
+#define ONEWIREDRIVER_INC_GPIO_WRAPPER_H_
+
+void AssertPin();
+void NegatePin();
+void SetPin2TxMode();
+void SetPin2RxMode();
+
+#endif /* ONEWIREDRIVER_INC_GPIO_WRAPPER_H_ */
---
+++ OneWireTest/UserModules/OneWireDriver/Src/GPIO_Wrapper.c
@@ -0,0 +1,25 @@
+/*
+ * GPIO_Wrapper.c
+ *
+ *  Created on: 2018/07/13
+ *      Author: Shuji
+ */
+#include "GPIO_Wrapper.h"
+
+__attribute__((weak)) void AssertPin()
+{
+
+}
+__attribute__((weak)) void NegatePin()
+{
+
+}
+__attribute__((weak)) void SetPin2TxMode()
+{
+
+}
+__attribute__((weak)) void SetPin2RxMode()
+{
+
+}
+
