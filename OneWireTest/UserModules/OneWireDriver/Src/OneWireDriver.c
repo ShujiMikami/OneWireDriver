262ecfbfe8d68be4ef8c251013ba4958001ad83d
ShujiMikami <carbonized.pan@gmail.com>
2018-07-13 03:20:12 +0900

    ResetPulseを実装

--- OneWireTest/UserModules/OneWireDriver/Src/OneWireDriver.c
+++ OneWireTest/UserModules/OneWireDriver/Src/OneWireDriver.c
@@ -4,5 +4,31 @@
  *  Created on: 2018/07/13
  *      Author: Shuji
  */
+#include "OneWireDriver.h"
 
+#include "OneWireSpecifications.h"
+#include "GPIO_Wrapper.h"
+#include "Wait_Wrapper.h"
 
+void ResetPulse()
+{
+	//GPIOをTxモードに
+	SetPin2TxMode();
+
+	//GPIOをアサート
+	AssertPin();
+
+	//tRSTHだけ待つ
+	Wait_us(T_RSTH);
+
+	//GPIOをネゲート
+	NegatePin();
+}
+ONE_WIRE_STATUS_t ReadPresensePulse()
+{
+	ONE_WIRE_STATUS_t result = ONE_WIRE_FAIL;
+
+	//TODO ReadPresensePulse実装
+
+	return result;
+}
